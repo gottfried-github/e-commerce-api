cb8dd8e8bdc395e5ae5a68d24678e770d64ca59d
osmomysl <dannvx@gmail.com>
2022-11-08 19:06:04 +0200

    use logger

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import {Router} from 'express'
 
+import {log, logger} from './logger.js'
+
 import admin from './routes/admin/admin.js'
 
 function api(store) {
     const router = Router()
+
+    /* attach logger to express */
+    router.use((req, res, next) => {
+        req.log = log.bind(logger)
+        next()
+    })
 
     router.use('/admin', admin(store))
 
