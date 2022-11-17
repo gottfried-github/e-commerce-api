d1d46ce132c08509d89eba7fb4f94b53b3878772
osmomysl <dannvx@gmail.com>
2022-11-17 20:21:44 +0200

    index: import other tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,23 @@
 import {testHandler} from './error-handler.js'
+import {testRoutes} from './auth-routes.js'
+import {testEnsureFieldsCreate, testEnsureFieldsUpdate, testMakeEnsureFields} from './product-helpers.js'
 
 describe("errorHandler", () => {
     testHandler()
+})
+
+describe("auth routes", () => {
+    testRoutes()
+})
+
+describe("ensureFieldsCreate", () => {
+    testEnsureFieldsCreate()
+})
+
+describe("ensureFieldsUpdate", () => {
+    testEnsureFieldsUpdate()
+})
+
+describe("testMakeEnsureFields", () => {
+    testMakeEnsureFields()
 })
