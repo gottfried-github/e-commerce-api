6c90d34f46302cd26f08f60ac55b43682331996d
osmomysl <dannvx@gmail.com>
2022-11-29 21:42:05 +0200

    client: refactor

--- src/client/auth.js
+++ src/client/auth.js
@@ -46,12 +46,4 @@ async function isAuthenticated(successCb, failureCb) {
     return successCb(body, res)
 }
 
-async function logout(successCb, failureCb) {
-    const res = await fetch('/api/admin/user/logout', {method: 'GET'})
-    const body = await res.json()
-
-    if (!res.ok && failureCb) return failureCb(body, res)
-    return successCb(body, res)
-}
-
-export {login, signup, isAuthenticated, logout}
+export {login, signup, isAuthenticated}
--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,6 @@
 import * as auth from './auth.js'
+import * as user from './user.js'
+
 export default {
-    ...auth
+    auth, user, 
 }
