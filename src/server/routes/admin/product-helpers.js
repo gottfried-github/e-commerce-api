1db55dc297b8b2a9592f6a398ace082065eca139
osmomysl <dannvx@gmail.com>
2022-12-13 14:58:07 +0200

    product, helpers: wrap errors in ValidationError; fix error generating code;

--- src/server/routes/admin/product-helpers.js
+++ src/server/routes/admin/product-helpers.js
@@ -4,29 +4,42 @@ import * as m from '../../../../../fi-common/messages.js'
  * @description deeply type-check the fields
 */
 function ensureFields(body) {
-    // NOTE: see Todo in EnsureFields behavior on additional properties
     const fields = productStripFields(body)
+    // const {expose, name, price, is_in_stock, photos_all, photos, cover_photo, description} = body
 
     const errors = {errors: [], node: {}}
 
-    if ('boolean' !== typeof fields.expose) errors.node.expose = {errors: [new TypeError("'isInSale' must be boolean")], node: null}
-    if ('string' !== typeof fields.name) errors.node.name = {errors: [new TypeError("'name' must be a string")], node: null}
-    if ('number' !== typeof fields.price) errors.node.price = {errors: [new TypeError("'price' must be a number")], node: null}
-    if ('boolean' !== typeof fields.is_in_stock) errors.node.is_in_stock = {errors: [new TypeError("'name' must be boolean")], node: null}
-    if (!Array.isArray(fields.photos)) errors.node.photos = {errors: [new TypeError("'photos' must be an array")], node: null}
-    if ('string' !== typeof fields.cover_photo) errors.node.coverPhoto = {errors: [new TypeError("'cover_photo' must be a string")], node: null}
-    if ('string' !== typeof fields.description) errors.node.description = {errors: [new TypeError("'description' must be a string")], node: null}
+    if (undefined !== fields.expose && 'boolean' !== typeof fields.expose) errors.node.expose = {errors: [new TypeError("'isInSale' must be boolean")], node: null}
+    if (undefined !== fields.name && 'string' !== typeof fields.name) errors.node.name = {errors: [new TypeError("'name' must be a string")], node: null}
+    if (undefined !== fields.price && 'number' !== typeof fields.price) errors.node.price = {errors: [new TypeError("'price' must be a number")], node: null}
+    if (undefined !== fields.is_in_stock && 'boolean' !== typeof fields.is_in_stock) errors.node.is_in_stock = {errors: [new TypeError("'name' must be boolean")], node: null}
+    if (undefined !== fields.photos_all && !Array.isArray(fields.photos_all)) errors.node.photos = {errors: [new TypeError("'photos' must be an array")], node: null}
+    if (undefined !== fields.photos && !Array.isArray(fields.photos)) errors.node.photos = {errors: [new TypeError("'photos' must be an array")], node: null}
+    if (undefined !== fields.cover_photo && 'string' !== typeof fields.cover_photo) errors.node.coverPhoto = {errors: [new TypeError("'cover_photo' must be a string")], node: null}
+    if (undefined !== fields.description && 'string' !== typeof fields.description) errors.node.description = {errors: [new TypeError("'description' must be a string")], node: null}
 
     if (Array.isArray(fields.photos)) {
-        for (const photo of fields.photos) {
+        for (const [i, photo] of fields.photos.entries()) {
             if ("string" !== typeof photo) {
                 if (!errors.node.photos) {errors.node.photos = {errors: [], node: []}}
-                errors.node.photos.node.push(new TypeError("children of 'photos' must be strings"))
+                errors.node.photos.node.push({index: i, errors: [new TypeError("children of 'photos' must be strings")], node: null})
+            }
+        }
+    }
+    
+    if (Array.isArray(fields.photos_all)) {
+        for (const [i, photo] of fields.photos_all.entries()) {
+            if ("string" !== typeof photo) {
+                if (!errors.node.photos_all) {errors.node.photos_all = {errors: [], node: []}}
+                errors.node.photos_all.node.push({index: i, errors: [new TypeError("children of 'photos' must be strings")], node: null})
             }
         }
     }
 
-    if (Object.keys(errors.node).length) return {fields: null, errors}
+    if (Object.keys(errors.node).length) return {
+        fields: null, 
+        errors: m.ValidationError.create('some fields are filled incorrectly', errors)
+    }
 
     return {fields}
 }
@@ -36,7 +49,13 @@ function ensureFields(body) {
  * @description `expose` is required when creating
 */
 function ensureFieldsCreate(body, {ensureFields}) {
-    if (!('expose' in body)) return {fields: null, errors: {errors: [], node: {expose: {errors: [m.FieldMissing.create("'expose' must be specified")], node: null}}}}
+    if (!('expose' in body)) return {
+        fields: null, 
+        errors: m.ValidationError.create('some fields are filled incorrectly', {
+            errors: [], node: {expose: {errors: [m.FieldMissing.create("'expose' must be specified")], node: null}}
+        })
+    }
+
     return ensureFields(body)
 }
 
@@ -46,7 +65,12 @@ function ensureFieldsCreate(body, {ensureFields}) {
 function ensureFieldsUpdate(body, {ensureFields}) {
     const fields = productStripFields(body)
     
-    if (!Object.keys(fields).length) return {fields: null, errors: {errors: [m.FieldMissing.create("at least one of the fields must be specified")], node: null}}
+    if (!Object.keys(fields).length) return {
+        fields: null, 
+        errors: m.ValidationError.create('some fields are filled incorrectly', {
+            errors: [m.FieldMissing.create("at least one of the fields must be specified")], node: null
+        })
+    }
 
     return ensureFields(body)
 }
