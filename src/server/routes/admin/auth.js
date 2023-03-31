a507ca92e6c6358ed2e116db38b056f482f94311
osmomysl <dannvx@gmail.com>
2023-03-31 21:31:14 +0300

    validate password on signup

--- src/server/routes/admin/auth.js
+++ src/server/routes/admin/auth.js
@@ -4,7 +4,7 @@ import {Router} from 'express'
 import {log, logger} from '../../logger.js'
 import * as m from '../../../../../fi-common/messages.js'
 
-import {ensureCredentials} from './auth-helpers.js'
+import {ensureCredentials, validatePsswd} from './auth-validate.js'
 
 function authenticate(req, res, next, {authenticate}) {
     return authenticate(req, res, next).then(_res => {
@@ -43,7 +43,15 @@ function auth(auth) {
     const router = Router()
 
     router.post('/login', bodyParser.urlencoded(), ensureCredentials, (req, res, next) => {authenticate(req, res, next, {authenticate: auth.authenticate})})
-    router.post('/signup', bodyParser.urlencoded(), ensureCredentials, (req, res, next) => {signup(req, res, next, {signup: auth.signup})})
+    router.post('/signup', bodyParser.urlencoded(), ensureCredentials, 
+        (req, res, next) => {
+            const errors = validatePsswd(req.body.password)
+            if (errors) return next(errors)
+
+            next()
+        }, 
+        (req, res, next) => {signup(req, res, next, {signup: auth.signup})}
+    )
     
     router.get('/is-authenticated', (req, res) => {
         req.log('/api/admin/auth/is-authenticated, req.user:', req.user)
