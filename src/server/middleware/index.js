538cfa6a6f9d9c599e5e2eafd98f981fb3572306
Dan <gottfried@gmail.com>
2023-10-28 16:45:12 +0300

    wip: add some missing middleware files

---
+++ src/server/middleware/index.js
@@ -0,0 +1,13 @@
+import admin from './admin/admin.js'
+import visitor from './visitor/visitor.js'
+import errorHandler from './error-handler.js'
+
+function main(services, options) {
+    return {
+        admin: admin(services, options),
+        visitor: visitor(),
+        common: {errorHandler}
+    }
+}
+
+export default main
