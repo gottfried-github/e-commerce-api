3174ec414d98f3bf35f566d3b67f5c2004136ff1
gottfried <gottfried.dev@gmail.com>
2023-12-28 15:26:48 +0200

    refactor

---
+++ src/server/services/admin/resources/resources.js
@@ -0,0 +1,11 @@
+import product from './product.js'
+
+function main(store, options) {
+    return {
+        product: product({
+            product: store.product, options
+        })
+    }
+}
+
+export default main
--- src/server/services/index.js
+++ src/server/services/index.js
@@ -1,10 +1,16 @@
-import storeAdmin from './admin/store.js'
+import storeAdmin from './admin/store/store.js'
+import resourcesAdmin from './admin/resources/resources.js'
 import storeVisitor from './visitor/store.js'
 
 function main(store, options) {
     return {
-        admin: storeAdmin(store, options),
-        visitor: storeVisitor(store)
+        store: {
+            admin: storeAdmin(store),
+            visitor: storeVisitor(store)
+        },
+        resources: {
+            admin: resourcesAdmin(store, options)
+        }
     }
 }
 
