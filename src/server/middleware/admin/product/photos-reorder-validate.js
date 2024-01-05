eec0f99e1c3f174e30b388991242431afb974f41
gottfried <gottfried.dev@gmail.com>
2024-01-05 15:22:02 +0200

    lib, middleware: photos reorder

--- src/server/middleware/admin/product/photos-reorder-validate.js
+++ src/server/middleware/admin/product/photos-reorder-validate.js
@@ -1,3 +1,10 @@
-export default () => function validate() {
-    
+import * as m from '../../../../../../e-commerce-common/messages.js'
+import validate from '../../../lib/admin/validate/product/photos-reorder.js'
+
+export default () => (req, res, next) => {
+    const errors = validate(req.body.photos)
+
+    if (!errors) next()
+
+    next(m.ValidationError.create("some photos are invalid", errors.node))
 }
