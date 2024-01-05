41db998155767b00f14faa8e6b03818e7775ead8
gottfried <gottfried.dev@gmail.com>
2024-01-05 15:29:24 +0200

    lib, middleware: validate set cover photo

---
+++ src/server/lib/admin/validate/product/photos-setCover.js
@@ -0,0 +1,37 @@
+import Ajv from 'ajv'
+import {toTree} from 'ajv-errors-to-data-tree'
+
+import * as m from '../../../../../../../e-commerce-common/messages.js'
+
+const ajv = new Ajv({allErrors: true, strictRequired: true})
+
+const schema = {
+    type: 'object',
+    properties: {
+        id: {
+            type: 'string'
+        },
+        cover: {
+            type: 'boolean'
+        }
+    },
+    required: ['id', 'cover'],
+    additionalProperties: false
+}
+
+const _validate = ajv.compile(schema)
+
+function validate(photos) {
+    if (_validate(photos)) return false
+
+    const errors = toTree(_validate.errors, (e) => {
+        if ('required' === e.keyword) return m.FieldMissing.create(e.message, e)
+        if ('type' === e.keyword) return m.TypeErrorMsg.create(e.message, e)
+
+        return m.ValidationError.create(e.message, null, e)
+    })
+
+    return errors
+}
+
+export {validate as default, _validate}
--- src/server/middleware/admin/product/photos-setCover-validate.js
+++ src/server/middleware/admin/product/photos-setCover-validate.js
@@ -1,3 +1,10 @@
-export default () => function validate() {
-    
+import * as m from '../../../../../../e-commerce-common/messages.js'
+import validate from '../../../lib/admin/validate/product/photos-setCover.js'
+
+export default () => (req, res, next) => {
+    const errors = validate(req.body.photo)
+
+    if (!errors) next()
+
+    next(m.ValidationError.create("photo failed validation", errors.node))
 }
